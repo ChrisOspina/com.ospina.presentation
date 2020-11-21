f44917a51e62e6fd787c805123f8cc2f46648286
Christopher Ospina <ospicg@farmingdale.edu>
2020-11-21 18:36:51 -0500

    reinstated JSON Methods

--- src/main/java/com/ospina/buisness/MyDate.java
+++ src/main/java/com/ospina/buisness/MyDate.java
@@ -5,86 +5,160 @@
  */
 package com.ospina.buisness;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.SerializedName;
 import java.io.FileReader;
 import java.io.PrintStream;
 
 /**
- * Contains code for the MyDate class. This class
- * represents a date in which a workers has taken a shift
- * the code will be reused in the Shift class through composition 
+ * Contains code for the MyDate class. This class represents a date in which a
+ * workers has taken a shift the code will be reused in the Shift class through
+ * composition
+ *
  * @author cospina
  * @since 9/22/20
  */
 public class MyDate {
+
+    @SerializedName("month")
     private int Month;
+    @SerializedName("day")
     private int Day;
+    @SerializedName("year")
     private int Year;
-    
-  /**
-   * This method is the 
-   * Default constructor for the MyDate class
-   * 
-   */
-   public MyDate(){}
-   
-   /**
-    * This method is the constructor for the MyDate class.
-    * Unlike the default constructor, the user sets his own values
-    * @param NewMonth represents a new month value that the user inputs
-    * @param NewDay represents a new day value that the user inputs
-    * @param NewYear represents a new year value that the user inputs
-    */
-   public MyDate(int NewMonth, int NewDay,int NewYear)
-   {
-       Month = NewMonth;
-       Day = NewDay;
-       Year = NewYear;
-   }
-   
-   /**
-    * This is the set method for 
-    * the "Month" member variable.
-    * @param m a placeholder for a new month value
-    */
-   public void setMonth(int m) {Month = m;}
-   
-   /**
-    * This is the set method for the
-    * "Day" member variable
-    * @param d a placeholder for a new day value
-    */
-   public void setDay(int d){Day = d;}
-   
-   /**
-    * This is the set method for the
-    * "Year" member variable
-    * 
-    * @param y a placeholder for a new year value
-    */
-   public void setYear(int y){Year = y;}
-   
-   /**
-    * This is the get method for the 
-    * "Year" member variable
-    * 
-    * @return the current value of "Year" 
-    */
-   public int getYear(){return Year;}
-   
-   /**
-    * This is the get method for the 
-    * "Day" member variable
-    * 
-    * @return the current value of "Day"
-    */
-   public int getDay() {return Day;}
-   
-   /**
-    * This is the get method for the 
-    * "Month" member variable
-    * 
-    * @return the current value of "Month"
-    */
-   public int getMonth() {return Month;}
-   
+
+    /**
+     * This method is the Default constructor for the MyDate class
+     *
+     */
+    public MyDate() {
+    }
+
+    /**
+     * This method is the constructor for the MyDate class. Unlike the default
+     * constructor, the user sets his own values
+     *
+     * @param NewMonth represents a new month value that the user inputs
+     * @param NewDay represents a new day value that the user inputs
+     * @param NewYear represents a new year value that the user inputs
+     */
+    public MyDate(int NewMonth, int NewDay, int NewYear) {
+        Month = NewMonth;
+        Day = NewDay;
+        Year = NewYear;
+    }
+
+    /**
+     * This is the set method for the "Month" member variable.
+     *
+     * @param m a placeholder for a new month value
+     */
+    public void setMonth(int m) {
+        Month = m;
+    }
+
+    /**
+     * This is the set method for the "Day" member variable
+     *
+     * @param d a placeholder for a new day value
+     */
+    public void setDay(int d) {
+        Day = d;
+    }
+
+    /**
+     * This is the set method for the "Year" member variable
+     *
+     * @param y a placeholder for a new year value
+     */
+    public void setYear(int y) {
+        Year = y;
+    }
+
+    /**
+     * This is the get method for the "Year" member variable
+     *
+     * @return the current value of "Year"
+     */
+    public int getYear() {
+        return Year;
+    }
+
+    /**
+     * This is the get method for the "Day" member variable
+     *
+     * @return the current value of "Day"
+     */
+    public int getDay() {
+        return Day;
+    }
+
+    /**
+     * This is the get method for the "Month" member variable
+     *
+     * @return the current value of "Month"
+     */
+    public int getMonth() {
+        return Month;
+    }
+
+    /**
+     * Reads the contents of all member variables from the given instance of
+     * FileReader as JSON.
+     *
+     * @param fr the FileReader instance that performs the reading of the values
+     * as JSON
+     *
+     */
+    public void readJSON(FileReader fr) {
+        //***********************
+        //In order to read to JSON we need a new instance
+        //of GSONBuilder. We also need to call the setPrettyPrinting
+        //and create methods while we are at it
+        //********************************************
+        GsonBuilder builder = new GsonBuilder();
+        builder.setPrettyPrinting();
+        Gson gson = builder.create();
+        //******************************************
+        //We then must create an instance of MyDate in order to use as
+        //a refrence for the JSON file to read
+        //****************************************
+        MyDate md = gson.fromJson(fr, MyDate.class);
+        //Finally we will set each member variable to the returned values from get
+        this.setMonth(md.getMonth());
+        this.setDay(md.getDay());
+        this.setYear(md.getYear());
+    }
+
+    /**
+     * Writes the member variables in JSON format to the given PrintStream
+     *
+     * @param ps the PrintStream instance that performs the reading of the
+     * values to JSON
+     */
+    public void writeJSON(PrintStream ps) {
+        //The same setup as above but we are writing to a JSON file
+        GsonBuilder builder = new GsonBuilder();
+        builder.setPrettyPrinting();
+        Gson gson = builder.create();
+        String jsonString = gson.toJson(this);
+        ps.println(jsonString);
+    }
+
+    /**
+     * This method creates a string representation of the MyDate object in
+     * proper date format
+     *
+     * @return the result of the ToString method
+     */
+    @Override
+    public String toString() {
+        //**********************************************************
+        //To create the string representation we must concatenate
+        //all member values of this class into proper date format
+        //**********************************************************
+        String s = this.Month + "/" + this.Day + "/" + this.Year;
+        return s;
+    }
 }
--- src/main/java/com/ospina/buisness/Timesheet.java
+++ src/main/java/com/ospina/buisness/Timesheet.java
@@ -5,6 +5,12 @@
  */
 package com.ospina.buisness;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.SerializedName;
+import java.io.FileReader;
+import java.io.PrintStream;
+
 /**
  * Contains code for the TimeSheetClass. This class represents
  * A Worker's Time sheet that documents their weekly shifts.
@@ -14,7 +20,9 @@ package com.ospina.buisness;
  * @since 10/21/20
  */
 public class Timesheet {
+    @SerializedName("worker")
     private Worker Worker;
+    @SerializedName("shifts")
     private Shift Shifts[];
     /**
      * This method is the default constructor for the
@@ -46,5 +54,66 @@ public class Timesheet {
      */
     public Worker getWorker(){return Worker;}
     
+     public void writeJSON(PrintStream ps)
+    {
+        //The same setup as above but we are writing to a JSON file
+        GsonBuilder builder = new GsonBuilder();
+        builder.setPrettyPrinting();
+        Gson gson = builder.create();
+        //*****************************************
+        //We also need to confirm that we're writing the curent instance 
+        //to the file and printing it
+        //**************************************       
+        String jsonString = gson.toJson(this);
+        ps.println(jsonString);
+    }
+    /**
+     * This method reads time sheet data from a JSON file
+     * @param fr 
+     */
+    public void readJSON(FileReader fr)
+    {
+        //*************************************************
+        //In order to read to JSON we need a new instance
+        //of GSONBuilder. We also need to call the setPrettyPrinting
+        //and create methods while we are at it
+        //********************************************
+        GsonBuilder builder = new GsonBuilder();
+        builder.setPrettyPrinting();
+        Gson gson = builder.create();
+        //******************************************
+        //We then must create an instance of Timesheet
+        //in order to use as
+        //a refrence for the JSON file to read
+        //****************************************
+        Timesheet t = gson.fromJson(fr, Timesheet.class);
+        //***********************************************
+        //This will set the current instance's member variables to 
+        //those of the JSON file
+        //*****************************************
+        this.setWorker(t.getWorker());
+        this.Shifts= t.Shifts;      
+    } 
+    /**
+     * This method converts the object data into a string 
+     * @return the String representation of the class
+     */
+    @Override
+    public String toString()
+    {
+        //First we need to take the Worker's first and last name
+        String s = Worker.getFirst() + " " + Worker.getLast() + "\n";
+        //***********************************************************
+        //Next this for loop will include the prewritten toString 
+        //representations of each instance of the shifts array
+        //*********************************************************
+        for(int i =0; i<Shifts.length;i++)
+        {  
+          s += Shifts[i].toString()+"\n";
+        }
+        //Finally we can return the concactenated string 
+        return s;       
+    }
+            
 }      
    
